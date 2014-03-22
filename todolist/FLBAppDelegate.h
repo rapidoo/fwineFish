697ade8d3b50e2d7d3f7f459dd12e0f75e26da29
Frederic Le Bris <flebris@gmail.com>
2014-03-22 19:15:15 +0100

    Initial Commit

---
+++ todolist/FLBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FLBAppDelegate.h
+//  todolist
+//
+//  Created by Frederic Le Bris on 22/03/2014.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FLBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ todolist/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  todolist
+//
+//  Created by Frederic Le Bris on 22/03/2014.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FLBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FLBAppDelegate class]));
+    }
+}
